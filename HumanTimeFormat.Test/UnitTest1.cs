766d47ccd2d54433263175a265757c20dd5d4456
Yi-Shiuan <amour99520@gmail.com>
2018-06-11 21:26:54 +0800

    second test fail

--- HumanTimeFormat.Test/UnitTest1.cs
+++ HumanTimeFormat.Test/UnitTest1.cs
@@ -12,17 +12,23 @@ namespace HumanTimeFormat.Test
         [SetUp]
         public void Setup()
         {
-            
+            humanTimer = new HumanTimer();
         }
         
         [Test]
         public void Input_0_ShouldBe_Now()
         {
-            var format = new HumanTimer();
-            var result = format.ToHumanTime(0);
+            var result = humanTimer.ToHumanTime(0);
 
             result.Should().Be("now");
         }
 
+        [Test]
+        public void Input_1_ShouldBe_1_second()
+        {
+            var result = humanTimer.ToHumanTime(1);
+
+            result.Should().Be("1 Second");
+        }
     }
 }
