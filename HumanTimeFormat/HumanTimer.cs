23c0d3bb2b95f2b4e296c4dad4dc3a29548f9504
Yi-Shiuan <amour99520@gmail.com>
2018-06-11 21:47:44 +0800

    four test passed

--- HumanTimeFormat/HumanTimer.cs
+++ HumanTimeFormat/HumanTimer.cs
@@ -4,6 +4,13 @@ namespace HumanTimeFormat.Test
     {
         public string ToHumanTime(int time)
         {
+            var result = string.Empty;
+            if (time >= 60)
+            {
+                var min = time / 60;
+                return $"{min} Minute{(min > 1 ? "s" : string.Empty)}";
+            }
+            
             if (time > 0)
             {
                 return $"{time} Second{(time > 1 ? "s" : string.Empty)}";
